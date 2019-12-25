d44bc221070bc42b0f850e5440f1ea54193719c3
Kenneth Black <kenneth.black@live.com>
2019-12-25 15:07:42 -0800

    add tests for games controller

--- server/src/games/games.controller.spec.ts
+++ server/src/games/games.controller.spec.ts
@@ -2,8 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GamesController } from './games.controller';
 import { GamesService } from "./games.service";
 
+import { GAMES } from '../../mocks/games.mock';
+
 describe('Games Controller', () => {
   let controller: GamesController;
+
+  const response = {
+    send: (body?: any) => { },
+    status: (code: number) => response,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,5 +23,36 @@ describe('Games Controller', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getGames', () => {
+    it('should return an array of games', async () => {
+      expect(await controller.getGames()).toBe(GAMES);
+    });
+  });
+
+  describe('getGame', () => {
+    it('should return the right game', async () => {
+      expect(await controller.getGame(1)).toBe(GAMES[0]);
+    });
+  });
+
+  describe('addGame', () => {
+    it('should properly add a game', async () => {
+      const newGame = {
+        id: 99,
+        title: 'Added Game',
+        description: 'Added game for testing'
+      };
+      await controller.addGame(newGame);
+      expect(await controller.getGame(99)).toBe(newGame);
+    });
+  });
+
+  describe('deleteGame', () => {
+    it('should delete a game', async () => {
+      await controller.deleteGame({gameID: 1});
+      await expect(controller.getGame(1)).rejects.toThrow();
+    });
   });
 });
