0e4654778a26b5f682d747c73d97b1703fb37d9d
Brigette Eckert <brigette.eckert@live.com>
2019-12-25 15:36:07 -0800

    Added tests for book module CRD properties

--- server/src/books/books.service.spec.ts
+++ server/src/books/books.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
 
+import { BOOKS } from '../../mocks/books.mock';
+
 describe('BooksService', () => {
   let service: BooksService;
 
@@ -14,5 +16,32 @@ describe('BooksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a list of all books', async () => {
+    const result = await service.getBooks();
+    expect(result).toBe(BOOKS);
+  });
+
+  it('should return the right book', async () => {
+      const result = await service.getBook(1);
+      expect(result).toBe(BOOKS[0]);
+  });
+
+  it('should add a book properly', async () => {
+      const newBook = {
+          id: 101,
+          title: '101st Book',
+          description: 'This is the description for the 101st book',
+          author: 'Nikola',
+      };
+      const result = await service.addBook(newBook);
+      expect(result[result.length -1]).toBe(newBook);
+  });
+
+  it('should delete a book properly', async () => {
+      await service.deleteBook(2);
+      const result = await service.getBooks();
+      expect(result.findIndex(r => r.id === 2)).toBe(-1);
   });
 });
