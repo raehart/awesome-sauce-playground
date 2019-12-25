a1c621fbafa65f52f2fa6119f295e7a1c75d76f3
Brigette Eckert <brigette.eckert@live.com>
2019-12-25 14:39:27 -0800

    NestJS-setup: setup books module following nestJS tutorial

--- server/src/books/books.module.ts
+++ server/src/books/books.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
-
 @Module({
-  controllers: [BooksController],
-  providers: [BooksService]
+    controllers: [BooksController],
+    providers: [BooksService]
 })
 export class BooksModule {}
